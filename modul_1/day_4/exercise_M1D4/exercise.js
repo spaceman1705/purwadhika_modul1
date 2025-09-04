d5c5a9d8432a8b50ef5935982a6d371c0665426c
spaceman1705 <awangdota2@gmail.com>
2025-09-04 17:27:28 +0700

    day_4

--- modul_1/day_4/exercise_M1D4/exercise.js
+++ modul_1/day_4/exercise_M1D4/exercise.js
@@ -32,4 +32,42 @@ function fizzBuzz(n) {
 }
 console.log(fizzBuzz(6));
 
+
 // create a function to calculate Body mass index (BMI)
+let weight = 120; // in kg
+let height = 1.75; // in meters
+function calculateBMI(weight, height) {
+    let bmi = weight / (height * height);
+    if (bmi < 18.5) {
+        return "Less weight";
+    } else if (bmi >= 18.5 && bmi < 24.9) {
+        return "Ideal";
+    } else if (bmi >= 25.0 && bmi < 29.9) {
+        return "Overweight";
+    } else if (bmi >= 30.0 && bmi <39.9 ) {
+        return "Very Overweight";
+    } else {
+        return "Obesity";
+    }
+}
+console.log(calculateBMI(weight, height));
+
+
+// write a function to remove all odd numbers in an array and return a new array that cointains only even numbers
+function removeOddNumbers(arr) {
+    let evenNumbers = [];
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] % 2 === 0) {
+            evenNumbers.push(arr[i]);
+        }
+    }
+    return evenNumbers;
+}
+console.log(removeOddNumbers([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]));
+
+
+// write a function to split a string and convert it into an array of words
+function stringToArray(str) {
+    return str.split(" ");
+}
+console.log(stringToArray("Hello world this is a test"));
