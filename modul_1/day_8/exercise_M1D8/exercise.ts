aeea09b054c58e94ee45d5b98b06f98cb20510b5
spaceman1705 <awangdota2@gmail.com>
2025-09-15 14:50:05 +0700

    update

--- modul_1/day_8/exercise_M1D8/exercise.ts
+++ modul_1/day_8/exercise_M1D8/exercise.ts
@@ -45,40 +45,40 @@ class ShootingGame {
         let round: number = 1;
 
         while (this.player1.health > 0 && this.player2.health > 0) {
-        console.log(`Round ${round}`);
+            console.log(`Round ${round}`);
 
-        console.log(`${this.player1.name}'s turn`);
-        this.player1.showStat();
-        const item1 = this.getRandomItem();
-        console.log(`${this.player1.name} got: Health +${item1.health}, Power +${item1.power}`);
-        this.player1.useItem(item1);
-        this.player1.showStat();
-        console.log(`${this.player1.name} shoots!`);
-        this.player2.damage(this.player1.power);
-        if (this.player2.health <= 0) {
-            console.log(`${this.player2.name} died!`);
-            break;
-        } else {
-            console.log(`${this.player2.name} survived...`);
+            console.log(`${this.player1.name}'s turn`);
+            this.player1.showStat();
+            const item1 = this.getRandomItem();
+            console.log(`${this.player1.name} got: Health +${item1.health}, Power +${item1.power}`);
+            this.player1.useItem(item1);
+            this.player1.showStat();
+            console.log(`${this.player1.name} shoots!`);
+            this.player2.damage(this.player1.power);
+            if (this.player2.health <= 0) {
+                console.log(`${this.player2.name} died!`);
+                break;
+            } else {
+                console.log(`${this.player2.name} survived...`);
+            }
+
+            console.log(`${this.player2.name}'s turn`);
+            this.player2.showStat();
+            const item2 = this.getRandomItem();
+            console.log(`${this.player2.name} got: Health +${item2.health}, Power +${item2.power}`);
+            this.player2.useItem(item2);
+            this.player2.showStat();
+            console.log(`${this.player2.name} shoots!`);
+            this.player1.damage(this.player2.power);
+            if (this.player1.health <= 0) {
+                console.log(`${this.player1.name} died!`);
+                break;
+            } else {
+                console.log(`${this.player1.name} survived...`);
+            }
+
+            round++;
         }
-
-        console.log(`${this.player2.name}'s turn`);
-        this.player2.showStat();
-        const item2 = this.getRandomItem();
-        console.log(`${this.player2.name} got: Health +${item2.health}, Power +${item2.power}`);
-        this.player2.useItem(item2);
-        this.player2.showStat();
-        console.log(`${this.player2.name} shoots!`);
-        this.player1.damage(this.player2.power);
-        if (this.player1.health <= 0) {
-            console.log(`${this.player1.name} died!`);
-            break;
-        } else {
-            console.log(`${this.player1.name} survived...`);
-        }
-
-        round++;
-    }
     console.log(`Winner: ${this.player1.health > this.player2.health ? this.player1.name : this.player2.name}!`);
   }
 }
